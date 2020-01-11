50da7208abce22c514910b47eef6a73ef7c42887
= <=>
2020-01-11 02:00:39 +0200

    topbar rework to functional component

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,20 +4,18 @@ import Search from "./Search";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 
-export default class TopBar extends Component {
-    render() {
-        const { theme } = this.props;
-        return (
-            <Navbar expand="md" style={{...style, backgroundColor: theme.primary}}>
-                <div style={{display: "flex", justifyContent: "space-between", width: '100%'}}>
-                    <NavbarBrand style={{color: theme.textPrimary, flex: 1}}>Yet Another Airplane Map</NavbarBrand>
-                    <Search isDarkMode={this.props.isDarkMode} style={{color: theme.textPrimary, backgroundColor: theme.primary}}/>
-                    <div style={{flex: 1}}></div>
-                </div>
-            </Navbar>
-        )
-    }
+export default function TopBar({theme, isDarkMode}) {
+    return (
+        <Navbar expand="md" style={{...style, backgroundColor: theme.primary}}>
+            <div style={{display: "flex", justifyContent: "space-between", width: '100%'}}>
+                <NavbarBrand style={{color: theme.textPrimary, flex: 1}}>Yet Another Airplane Map</NavbarBrand>
+                <Search isDarkMode={isDarkMode} style={{color: theme.textPrimary, backgroundColor: theme.primary}}/>
+                <div style={{flex: 1}}></div>
+            </div>
+        </Navbar>
+    )
 }
+
 
 const style = {
     paddingBottom: 0,
